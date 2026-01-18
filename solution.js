43e5eb9f0885e650c4d1b56f6b86e5185b80accf
Ashish-1052 <akedari1052@gmail.com>
2026-01-18 20:37:14 +0530

    minor change

--- solution.js
+++ solution.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "family_travel_tracker",
-  password: "Ask@NSK1052!?",
+  password: "****",
   port: 5432,
 });
 db.connect();
